59a3965bc0bdd74baf2d3bef1826f88a77176283
Gabyrocha <gabsfoxy@gmail.com>
2022-01-14 07:33:35 -0300

    Adiciona repositório DotNet

---
+++ Classes/Herança/Ponto.cs
@@ -0,0 +1,27 @@
+﻿namespace Classes.Herança
+{
+    public class Ponto
+    {
+        public int x, y;
+        private int distancia;
+        
+        public Ponto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        protected void CalcularDistancia()
+        {
+            // Faz algo
+        }
+        private void CalcularDistancia2()
+        {
+            // Faz algo
+        }
+        public virtual void CalcularDistancia3()
+        {
+            // Faz algo
+        }
+    }
+}
---
+++ Classes/Métodos/Ref.cs
@@ -0,0 +1,18 @@
+namespace Métodos
+{
+    public class Ref
+    {
+        static void Inverter( ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        public static void Inverter()
+        {
+            int i = 1, j = 2;
+            Inverter(i, j);
+            System.Console.WriteLine($"{i} {j}"); // Escreve "2 1"
+        }
+    }
+}
